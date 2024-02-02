2950e3de0855bdfdf21cf1b0934a64bcc6be7006
abr-ya <gamer05@list.ru>
2024-02-03 00:05:53 +0300

    feat: Add Search component

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./search.module.css";
+import { faBed, faCalendarDays } from "@fortawesome/free-solid-svg-icons";
 
 const Search = () => {
-  return <div className={styles.search}>Search</div>;
+  const [destination, setDestination] = useState("");
+
+  const handleSearch = () => console.log(destination);
+
+  return (
+    <div className={styles.search}>
+      <div className={styles.searchItem}>
+        <FontAwesomeIcon icon={faBed} className={styles.searchIcon} />
+        <input
+          type="text"
+          placeholder="Where are you going?"
+          className={styles.searchInput}
+          onChange={(e) => setDestination(e.target.value)}
+        />
+      </div>
+      <div className={styles.searchItem}>
+        <FontAwesomeIcon icon={faCalendarDays} className={styles.searchIcon} />
+        {/* DateRange */}
+      </div>
+      {/* Person */}
+      <div className={styles.searchItem}>
+        <button className={styles.searchBtn} onClick={handleSearch}>
+          Search
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
