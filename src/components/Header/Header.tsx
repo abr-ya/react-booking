bc6396eb179abfdf7d6c7f3a7b9e3b6ba0ef2338
abr-ya <gamer05@list.ru>
2024-02-03 16:19:04 +0300

    feat: Add short mode to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,28 @@
+import { FC } from "react";
+import cn from "classnames";
+
 import Menu from "./Menu";
 import Search from "./Search";
 import styles from "./header.module.css";
 
-const Header = () => (
+interface IHeader {
+  withSearch: boolean;
+}
+
+const Header: FC<IHeader> = ({ withSearch }) => (
   <div className={styles.header}>
-    <div className={styles.headerContainer}>
+    <div className={cn(styles.headerContainer, { [styles.full]: withSearch })}>
       <Menu />
-      <h1 className={styles.headerTitle}>A lifetime of discounts? It's Genius.</h1>
-      <p className={styles.headerDesc}>
-        Get rewarded for your travels – unlock instant savings of 10% or more with a free Lamabooking account
-      </p>
-      <button className={styles.headerBtn}>Sign in / Register</button>
-      <Search />
+      {withSearch && (
+        <>
+          <h1 className={styles.headerTitle}>A lifetime of discounts? It's Genius.</h1>
+          <p className={styles.headerDesc}>
+            Get rewarded for your travels – unlock instant savings of 10% or more with a free Lamabooking account
+          </p>
+          <button className={styles.headerBtn}>Sign in / Register</button>
+          <Search />
+        </>
+      )}
     </div>
   </div>
 );
--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,15 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { Header, Navbar } from "../components";
 
 import styles from "./layout.module.css";
 
-const Layout = () => (
-  <div className={styles.layout}>
-    <Navbar />
-    <Header />
-    <Outlet />
-  </div>
-);
+const Layout = () => {
+  const { pathname } = useLocation();
+  console.log(pathname);
+
+  return (
+    <div className={styles.layout}>
+      <Navbar />
+      <Header withSearch={pathname === "/"} />
+      <Outlet />
+    </div>
+  );
+};
 
 export default Layout;
