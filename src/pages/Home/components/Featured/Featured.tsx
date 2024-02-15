b5276a32c00536115d90464c3d0628514be5606c
abr-ya <gamer05@list.ru>
2024-02-15 17:28:47 +0300

    feature: Add useFetch hook

--- src/pages/Home/components/Featured/Featured.tsx
+++ src/pages/Home/components/Featured/Featured.tsx
@@ -4,19 +4,29 @@ import { featuredData } from "@/data/featured";
 import { IMG_BASE_PATH1 } from "@/constants";
 
 import styles from "./featured.module.css";
+import useFetch from "@/hooks/useFetch";
 
-const Featured: FC = () => (
-  <div className={styles.featured}>
-    {featuredData.map((feat) => (
-      <div className={styles.featuredItem}>
-        <img src={`${IMG_BASE_PATH1}${feat.imgSrc}`} alt={feat.title} className={styles.featuredImg} />
-        <div className={styles.featuredTitles}>
-          <h1>{feat.title}</h1>
-          <h2>{feat.count} properties</h2>
+const Featured: FC = () => {
+  const API_URL = import.meta.env.VITE_API_URL;
+  const { data, loading } = useFetch(`${API_URL}hotels/`); // countByCity?cities=milan,vilnius,prague
+
+  if (loading) return "Loading please wait..."; // todo: loader
+
+  console.log(data);
+
+  return (
+    <div className={styles.featured}>
+      {featuredData.map((feat) => (
+        <div className={styles.featuredItem}>
+          <img src={`${IMG_BASE_PATH1}${feat.imgSrc}`} alt={feat.title} className={styles.featuredImg} />
+          <div className={styles.featuredTitles}>
+            <h1>{feat.title}</h1>
+            <h2>{feat.count} properties</h2>
+          </div>
         </div>
-      </div>
-    ))}
-  </div>
-);
+      ))}
+    </div>
+  );
+};
 
 export default Featured;
