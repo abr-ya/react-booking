8ba86e2f58bacdeefed8c35fa6b9377720981d3a
abr-ya <gamer05@list.ru>
2024-12-30 15:01:07 +0300

    feat: Add Rooms data to Reserve Modal

--- src/components/ReserveModal/ReserveModal.tsx
+++ src/components/ReserveModal/ReserveModal.tsx
@@ -14,17 +14,54 @@ interface IReserveModal {
 const ReserveModal: FC<IReserveModal> = ({ closeModal, data }) => {
   console.log("Rooms:", data);
 
-  const handleClick = async () => {
+  const isRoomAvailable = () => {
+    // todo date checking! roomNumber: IRoomNumber
+    const isFoundBookings = false;
+
+    return !isFoundBookings;
+  };
+
+  const handleRoomClick = (e: { target: HTMLInputElement }) => {
+    const checked = e.target.checked;
+    const value = e.target.value;
+    console.log(checked, value);
+  };
+
+  const handleReserveClick = async () => {
     console.log("Reserve Now click");
   };
 
   return (
     <div className={styles.reserve}>
-      <div className={styles.rContainer}>
-        <FontAwesomeIcon icon={faCircleXmark} className={styles.rClose} onClick={() => closeModal()} />
+      <div className={styles.container}>
+        <FontAwesomeIcon icon={faCircleXmark} className={styles.close} onClick={() => closeModal()} />
         <span>Select your rooms:</span>
-        todo: Map and Show Hotel Rooms!
-        <button onClick={handleClick} className={styles.button}>
+        {data.map((item) => (
+          <div className={styles.item} key={item._id}>
+            <div className={styles.itemInfo}>
+              <div className={styles.title}>{item.title}</div>
+              <div className={styles.desc}>{item.desc}</div>
+              <div className={styles.max}>
+                Max people: <b>{item.maxPeople}</b>
+              </div>
+              <div className={styles.price}>{item.price}</div>
+            </div>
+            <div className={styles.selectRooms}>
+              {item.roomNumbers.map((roomNumber) => (
+                <div className={styles.room} key={roomNumber._id}>
+                  <label>{roomNumber.number}</label>
+                  <input
+                    type="checkbox"
+                    value={roomNumber._id}
+                    onChange={handleRoomClick}
+                    disabled={!isRoomAvailable()}
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
+        <button onClick={handleReserveClick} className={styles.button}>
           Reserve Now!
         </button>
       </div>
